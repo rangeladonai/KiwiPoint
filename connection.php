036c7b55f9d9af00d4aff07f51a1668b2e77b319
rangeladonai <kohei_adonai74739@outlook.com>
2023-05-01 21:17:15 -0300

    Implementação funcional Docker

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_DATABASE');
-$user = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
+$host = 'db';
+$port = '3306';
+$dbname = 'Letrim';
+$user = 'root';
+$password = 'root';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password, [
