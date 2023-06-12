19a1be0b18291f38798294f11095ca34035fcba4
danielbrazz <daniel.brazz02@gmail.com>
2023-06-12 19:22:15 -0300

    Ajustado front cadastro empresa e login

--- view/cadastraEmpresa.php
+++ view/cadastraEmpresa.php
@@ -12,6 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <!--CSS. Cole aqui embaixo o CSS proprio da pagina-->
     <link rel="stylesheet" href="../css/cadastraEmpresa.css">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
     <div class="container">
